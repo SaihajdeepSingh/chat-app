d033e0c76f799fec839ae52038c8072a9a872e04
SaihajdeepSingh <saihajdeep013@gmail.com>
2026-05-19 15:20:33 +0530

    Updated lib/prisma

--- backend/lib/prisma.js
+++ backend/lib/prisma.js
@@ -2,8 +2,18 @@ const { PrismaClient } = require('@prisma/client');
 
 const prisma = global.__prisma ?? new PrismaClient({
   log: process.env.NODE_ENV === 'development' ? ['error', 'warn'] : ['error'],
+  datasources: {
+    db: {
+      url: process.env.DATABASE_URL,
+    },
+  },
 });
 
 if (process.env.NODE_ENV !== 'production') global.__prisma = prisma;
+
+prisma.$on('error', async () => {
+  await prisma.$disconnect();
+  await prisma.$connect();
+});
 
 module.exports = prisma;
